3af4d6e1d3a116f3392a67999805cf19e934721a
Harshita Pal <beingharshita280@gmail.com>
2022-10-06 15:47:27 +0530

    list functions added

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,14 +3,24 @@ import Map from '../Map/Map'
 import './styles.css'
 import Fab from '@mui/material/Fab';
 import AddIcon from '@mui/icons-material/Add';
-// import $ from "jquery";
+import $ from "jquery";
+import Header from "../Header/Header";
+import { Component } from "react";
 
-export default function List() {
+
+export default class List extends Component {
+    shouldComponentUpdate() {    
+        return false
+      }
+
+      
+  render() {
     const icon = '<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAABmJLR0QA/wD/AP+gvaeTAAAAu0lEQVRIie2VQQrCMBBFf9z1Ap7BRV3ZpV7HW3ZV0INUkPYSz80siiZI61gi5kMgTJL/JpOQSEW5CrjyqouXeRcxX6ouxti4ZPpmHyswfkAhNQC4lCiEEGWsccZRFXC24KOkRtI4iY0WO7lklHiFDjZWA4O12mJNbIEXeAqqn/rDN8EAI7CfzNsB99TklL/X5fJ7j3MrtdvlmlvqCmgktZK21lqLVXOMyieRBfjm4N8vAZ8/hPfmUfSnegA2268orRY1KwAAAABJRU5ErkJggg==">'
 
     var i = 1;
-
     return (
+    <>
+        <Header/>
         <div id="listbody">
             <div id="list">
                 <div className="lihead">
@@ -27,6 +37,7 @@ export default function List() {
             </div>
             <Map/>
         </div>
+        </>
     )
 
     function add (){
@@ -36,18 +47,28 @@ export default function List() {
         const place = document.getElementById('place').value
         var item = document.getElementById('items')
         const li = document.createElement('li')
-        li.setAttribute('id', 'listitem'+i)   
-        li.setAttribute('class', 'listitem')   
+        $(li).attr('id', 'listitem'+i)   
+        $(li).attr('class', 'listitem')   
         const del = document.createElement('span')
         const data = document.createTextNode(place)
 
         li.appendChild(rad)
         li.appendChild(data)
         del.innerHTML += icon
-        del.setAttribute('class', 'icon')     
+        $(del).attr('class', 'icon')   
+        $(del).attr('id', 'icon'+i) 
+        $(del).on("click", function(){
+            $(li).hide(1000)
+            $(li).remove(li)
+        })
+        $(rad).on("click", function(){
+            $(li).fadeOut(1000)
+            $(li).remove(li)
+        })
         li.appendChild(del)
         item.appendChild(li)
         document.getElementById('place').value = ''
+        
         i = i+1        
     }
     
@@ -59,8 +80,6 @@ export default function List() {
         }
       }
 
-      function Delete(){
-        document.getElementById('list').removeChild(document.getElementById('add'))
-        alert('go')
-      }
+}
+
 }
